dfeeea87f723c141de85fab182bf010e532e8ca6
Mike Huang <wellwind@mail2000.com.tw>
2023-05-27 16:47:08 +0800

    實戰演練：建立登入頁面

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,41 +1,52 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'dashboard',
-    pathMatch: 'full',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      },
+      {
+        path: 'charts',
+        // component: ChartsComponent
+        loadComponent: () =>
+          import('./charts/charts.component').then((c) => c.ChartsComponent),
+      },
+      {
+        path: 'tables',
+        loadComponent: () =>
+          import('./tables/tables.component').then((t) => t.TablesComponent),
+      },
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./dashboard/dashboard.component'),
+      },
+      {
+        path: 'utilities', // utilities/*
+        loadChildren: () => import('./utilities/utilities.routes'),
+      },
+      {
+        path: 'demo',
+        loadComponent: () =>
+          import('./demo/demo.component').then((d) => d.DemoComponent),
+      },
+      {
+        path: 'not-found',
+        loadComponent: () =>
+          import('./not-found/not-found.component').then(
+            (n) => n.NotFoundComponent
+          ),
+      },
+    ]
   },
   {
-    path: 'charts',
-    // component: ChartsComponent
-    loadComponent: () =>
-      import('./charts/charts.component').then((c) => c.ChartsComponent),
-  },
-  {
-    path: 'tables',
-    loadComponent: () =>
-      import('./tables/tables.component').then((t) => t.TablesComponent),
-  },
-  {
-    path: 'dashboard',
-    loadComponent: () => import('./dashboard/dashboard.component'),
-  },
-  {
-    path: 'utilities', // utilities/*
-    loadChildren: () => import('./utilities/utilities.routes'),
-  },
-  {
-    path: 'demo',
-    loadComponent: () =>
-      import('./demo/demo.component').then((d) => d.DemoComponent),
-  },
-  {
-    path: 'not-found',
-    loadComponent: () =>
-      import('./not-found/not-found.component').then(
-        (n) => n.NotFoundComponent
-      ),
+    path: 'login',
+    loadComponent: () => import('./login/login.component').then((l) => l.LoginComponent),
   },
   {
     path: '**',
