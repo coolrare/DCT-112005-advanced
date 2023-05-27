24a1b6ff667ab82185a844c84a636a85d2b1fdda
Mike Huang <wellwind@mail2000.com.tw>
2023-05-27 15:33:07 +0800

    補充退訂閱的機制

--- src/app/utilities/colors/colors.component.ts
+++ src/app/utilities/colors/colors.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { Observable, Subscription, interval, map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-colors',
@@ -9,15 +10,34 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './colors.component.html',
   styleUrls: ['./colors.component.css'],
 })
-export class ColorsComponent implements OnInit {
+export class ColorsComponent implements OnInit, OnDestroy {
   route = inject(ActivatedRoute);
   // type: string | null = null;
   type = '';
+
+  time = new Date();
+  timer = interval(1000);
+  subscription: Subscription | undefined;
+
+  timer$ = interval(1000).pipe(
+    tap(n => console.log(n)),
+    map(_ => new Date())
+  );
+
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
       // console.log(params.get('type'));
       this.type = params.get('type') || '';
     });
+
+    // this.subscription = this.timer.subscribe((n) => {
+    //   console.log(n);
+    //   this.time = new Date();
+    // });
+  }
+
+  ngOnDestroy(): void {
+    // this.subscription?.unsubscribe();
   }
 }
