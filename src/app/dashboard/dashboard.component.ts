94409f3bff8b28377841f780ab4f5080a261b130
Mike Huang <wellwind@mail2000.com.tw>
2023-05-27 14:50:38 +0800

    實戰演練：各種導覽功能撰寫

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { chartAreaDemo } from '../chart-area-demo';
 import { chartPieDemo } from '../chart-pie-demo';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  router = inject(Router);
+
   ngOnInit(): void {
     chartPieDemo();
     chartAreaDemo();
   }
 
+  goToTables() {
+    this.router.navigate(['/tables'], { queryParams: { num: 3 } });
+  }
 }
