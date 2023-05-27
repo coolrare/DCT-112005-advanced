5ac022f2aeaee863d3c6cbe331f4f00ca0f8e899
Mike Huang <wellwind@mail2000.com.tw>
2023-05-27 09:42:20 +0800

    實戰演練：套用多頁面路由與元件 (1)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ChartsComponent } from './charts/charts.component';
+import { TablesComponent } from './tables/tables.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'charts',
+    component: ChartsComponent
+  },
+  {
+    path: 'tables',
+    component: TablesComponent
+  }
+
+];
