e2e8672822bd7f49ae2f49a25a4f0c98b7628a0e
Mike Huang <wellwind@mail2000.com.tw>
2023-06-03 13:27:33 +0800

    練習使用 Model-driven form 建立表單模型，並與畫面上的 input 等繫結

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  formBuilder = inject(FormBuilder);
 
+  form = this.formBuilder.group({
+    firstName: this.formBuilder.control('Mike'),
+    lastName: this.formBuilder.control('Huang'),
+    email: this.formBuilder.control(''),
+    password: this.formBuilder.control(''),
+    confirmPassword: this.formBuilder.control(''),
+  });
+
+  register() {
+    console.log(this.form.value);
+    console.log('firstName (form.get)', this.form.get('firstName')?.value);
+    console.log('firstName (form.controls)', this.form.controls.firstName.value);
+  }
 }
