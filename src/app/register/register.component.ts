bde7145261efbb63d30ac9a71042dde45d7c0a67
Mike Huang <wellwind@mail2000.com.tw>
2023-06-03 14:27:38 +0800

    練習使用 FormGroup 和 FormArray 調整表單的模型結構

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -9,19 +9,33 @@ import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  formSubmitted = false;
   formBuilder = inject(FormBuilder);
 
   form = this.formBuilder.group({
     firstName: this.formBuilder.control('Mike'),
     lastName: this.formBuilder.control('Huang'),
     email: this.formBuilder.control(''),
-    password: this.formBuilder.control(''),
-    confirmPassword: this.formBuilder.control(''),
+    password: this.formBuilder.group({
+      password: this.formBuilder.control(''),
+      confirmPassword: this.formBuilder.control(''),
+    }),
+    skills: this.formBuilder.array([
+      this.formBuilder.control('HTML'),
+      this.formBuilder.control('CSS'),
+      this.formBuilder.control('JavaScript'),
+      this.formBuilder.control('Angular'),
+    ])
   });
 
   register() {
+    this.form.controls.skills.controls
+
+
+    let fieldName = 'firstName';
+    this.formSubmitted = true;
     console.log(this.form.value);
-    console.log('firstName (form.get)', this.form.get('firstName')?.value);
+    console.log('firstName (form.get)', this.form.get(fieldName)?.value);
     console.log('firstName (form.controls)', this.form.controls.firstName.value);
   }
 }
